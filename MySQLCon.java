3974180a19fea679bf62dd7a9d7a2affed8408ad
Leandro Coutinho <lescoutinhovr@gmail.com>
2022-05-28 09:44:23 -0300

    Test Prepared Statement with final string

--- MySQLCon.java
+++ MySQLCon.java
@@ -11,6 +11,25 @@ class MysqlCon {
 			ResultSet rs = stmt.executeQuery("select id, name from Person");
 			while(rs.next())
 				System.out.println(rs.getInt(1) + "  " + rs.getString(2));
+
+			pl("----------------------------------");
+			pl("Test Prepared Statement with final string");
+			// https://docs.oracle.com/javase/tutorial/jdbc/basics/prepared.html
+			final String query2 = """
+				select id, name
+				from Person
+				where id = ?
+			""";
+
+			PreparedStatement psId2 = con.prepareStatement(query2);
+			psId2.setInt(1, 2);
+			rs = psId2.executeQuery();
+			while(rs.next())
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2));
 		}
+	}
+
+	public static void pl(Object o) {
+		System.out.println(o);
 	}
 }
