1c14fb32fb1d688c3e5ef5ac49199e92aba41b0c
Leandro Coutinho <lescoutinhovr@gmail.com>
2022-06-03 19:49:14 -0300

    Add class Structure to handle Record and Class

--- com/github/mapresultset/MappingProcessor.java
+++ com/github/mapresultset/MappingProcessor.java
@@ -46,6 +46,7 @@ public class MappingProcessor extends AbstractProcessor {
 	private Set<Element> annotatedElements = new HashSet<>(); // TODO this is probably not necessary
 	private List<Element> tables = new ArrayList<>();
 	private List<Element> queries = new ArrayList<>();
+	private List<Structure> structures = new ArrayList<>();
 
 	@Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
@@ -57,154 +58,159 @@ public class MappingProcessor extends AbstractProcessor {
         return SourceVersion.latestSupported();
     }
 
+	public void processLastRound(final Set<? extends TypeElement> annotations, final RoundEnvironment roundEnvironment) {
+		/*
+			Last round ... we already collected all tables and queries
+		*/
+		System.out.println(">>>>>>>>>>>> Last round! Fight! <<<<<<<<<<<<");
+		System.out.println("tables.: " + tables);
+		System.out.println("queries: " + queries);
+		System.out.println("structures: " + structures);
+
+
+		// Map Table name -> to Class (including package)
+		Map<String, String> tableMap = new HashMap<>();
+		for (var te : tables) {
+			final String packageAndClass = te.toString();
+			int ld = packageAndClass.lastIndexOf(".");
+			String tableName;
+			if (ld == -1) {
+				tableName = packageAndClass;
+			} else {
+				tableName = packageAndClass.substring(ld + 1);
+			}
+			
+			for (var am : te.getAnnotationMirrors()) {
+				for (var entry : am.getElementValues().entrySet()) {
+					// TODO maybe there is a better way instead of using toString
+					if (entry.getKey().toString().equals("name()")) {
+						tableName = entry.getValue().getValue().toString();
+					}
+				}
+			}
+
+			if (tableMap.get(tableName) != null) {
+				throw new RuntimeException("It can't map " + packageAndClass + " to " + tableName +
+						", because class '" + tableMap.get(tableName) +
+						"' is already mapped to that table.");
+			}
+			tableMap.put(tableName, packageAndClass);
+		}
+
+		System.out.println("------ map: Table -> Class -------");
+		System.out.println(tableMap);
+
+		// It will create one MapResultSet class in each package that
+		// contains a @Query
+		// map: package -> list of methods ?
+		Map<String, List<String>> packageMethods = new HashMap<>();
+		packageMethods.put(ANONYMOUS_TABLE, new ArrayList<>());
+
+		List<ClassToCreate> listClassesToCreate = new ArrayList<>();
+		
+		// Parse all queries
+		for (var queryElement : queries) {
+			final String queryName = queryElement.toString();
+			List<String> generatedColumns = new ArrayList<>();
+			Set<String> queryImports = new HashSet<>();
+			String queryImportsStr = "";
+			final String packageName = getPackageName(queryElement);
+			final String queryClassName = getQueryClassName(queryName);
+			final String generatedColumnsClassName = uppercaseFirstLetter(queryName) + "GeneratedColumns";
+			String classContent = """
+					package %s;
+
+					import java.util.ArrayList;
+					import java.util.List;
+
+					#import#
+
+					public class %s {
+
+						
+					""".formatted(packageName, queryClassName);
+			var queryClassToCreate = new ClassToCreate(packageName, queryClassName, classContent);
+			listClassesToCreate.add(queryClassToCreate);
+			String query = (String) ((VariableElement) queryElement).getConstantValue();
+			System.out.println("-->> Parsing query: " + query);
+			var p = new ParseQuery(query);
+			p.parse();
+			System.out.println("p.getTables() = " + p.getTables());
+
+			for (var column : p.getColumns()) {
+				if (column.isGeneratedValue()) {
+					String alias = column.alias();
+					generatedColumns.add(alias);
+				} else {
+					String table = column.table();
+					if (table == null) {
+						// if it's not a generated column and table is null
+						// then it means the from clause must have a single table
+						if (p.getTables().size() > 1) {
+							throw new RuntimeException("Columns must be preceded by the table name " +
+									"when there are more than one table in the 'from' clause.");
+						}
+						for (var es : p.getTables().entrySet()) table = es.getValue();
+					} else {
+						table = p.getTables().get(table);
+					}
+					System.out.println("table = " + table + ", tableMap.get(table) = " + tableMap.get(table));
+					if (queryImports.add(tableMap.get(table))) {
+						queryImportsStr += "import " + tableMap.get(table) + ";\n";
+						String classTableName = splitPackageClass(tableMap.get(table))[1];
+						String content = """
+								public List<%s> list%s = new ArrayList<>();
+							""".formatted(classTableName, classTableName);
+						queryClassToCreate.content += content;
+					}
+				}
+			}
+
+			if (!generatedColumns.isEmpty()) {
+				queryClassToCreate.content += """
+						public List<%s> generatedColumns = new ArrayList<>();
+					""".formatted(generatedColumnsClassName);
+			}
+
+			queryClassToCreate.content += "}";
+			queryClassToCreate.content = queryClassToCreate.content.replaceAll("#import#", queryImportsStr);
+
+			System.out.println(queryClassToCreate.content);
+			writeBuilderFile(queryClassToCreate);
+
+			if (!generatedColumns.isEmpty()) {
+				// Create GeneratedColumns class
+
+				classContent = """
+					package %s;
+
+					import java.util.List;
+
+					public class %s {
+
+						
+					""".formatted(packageName, generatedColumnsClassName);
+				var generatedColumnsClassToCreate = new ClassToCreate(packageName, generatedColumnsClassName, classContent);
+
+				for (var field : generatedColumns) {
+					String content = "\tpublic Object " + field + ";\n";
+					generatedColumnsClassToCreate.content += content;
+				}
+				generatedColumnsClassToCreate.content += "}";
+				System.out.println(generatedColumnsClassToCreate.content);
+				writeBuilderFile(packageName, generatedColumnsClassName, generatedColumnsClassToCreate.content);
+			}
+
+			// Create map file
+			final String content = createMapResultSetClassForQuery(queryName, packageName, queryClassName, queryImports);
+			writeBuilderFile(packageName, "MapResultSet", content);
+		}
+	}
+
     @Override
     public boolean process(final Set<? extends TypeElement> annotations, final RoundEnvironment roundEnvironment) {
 		if ( roundEnvironment.processingOver() ) {
-			/*
-				Last round ... we already collected all tables and queries
-			*/
-			System.out.println(">>>>>>>>>>>> Last round! Fight! <<<<<<<<<<<<");
-			System.out.println("tables.: " + tables);
-			System.out.println("queries: " + queries);
-
-
-			// Map Table name -> to Class (including package)
-			Map<String, String> tableMap = new HashMap<>();
-			for (var te : tables) {
-				final String packageAndClass = te.toString();
-				int ld = packageAndClass.lastIndexOf(".");
-				String tableName;
-				if (ld == -1) {
-					tableName = packageAndClass;
-				} else {
-					tableName = packageAndClass.substring(ld + 1);
-				}
-				
-				for (var am : te.getAnnotationMirrors()) {
-					for (var entry : am.getElementValues().entrySet()) {
-						// TODO maybe there is a better way instead of using toString
-						if (entry.getKey().toString().equals("name()")) {
-							tableName = entry.getValue().getValue().toString();
-						}
-					}
-				}
-
-				if (tableMap.get(tableName) != null) {
-					throw new RuntimeException("It can't map " + packageAndClass + " to " + tableName +
-							", because class '" + tableMap.get(tableName) +
-							"' is already mapped to that table.");
-				}
-				tableMap.put(tableName, packageAndClass);
-			}
-
-			System.out.println("------ map: Table -> Class -------");
-			System.out.println(tableMap);
-
-			// It will create one MapResultSet class in each package that
-			// contains a @Query
-			// map: package -> list of methods ?
-			Map<String, List<String>> packageMethods = new HashMap<>();
-			packageMethods.put(ANONYMOUS_TABLE, new ArrayList<>());
-
-			List<ClassToCreate> listClassesToCreate = new ArrayList<>();
-			
-			// Parse all queries
-			for (var queryElement : queries) {
-				final String queryName = queryElement.toString();
-				List<String> generatedColumns = new ArrayList<>();
-				Set<String> queryImports = new HashSet<>();
-				String queryImportsStr = "";
-				final String packageName = getPackageName(queryElement);
-				final String queryClassName = getQueryClassName(queryName);
-				final String generatedColumnsClassName = uppercaseFirstLetter(queryName) + "GeneratedColumns";
-				String classContent = """
-						package %s;
-
-						import java.util.ArrayList;
-						import java.util.List;
-
-						#import#
-
-						public class %s {
-
-							
-						""".formatted(packageName, queryClassName);
-				var queryClassToCreate = new ClassToCreate(packageName, queryClassName, classContent);
-				listClassesToCreate.add(queryClassToCreate);
-				String query = (String) ((VariableElement) queryElement).getConstantValue();
-				System.out.println("-->> Parsing query: " + query);
-				var p = new ParseQuery(query);
-				p.parse();
-				System.out.println("p.getTables() = " + p.getTables());
-
-				for (var column : p.getColumns()) {
-					if (column.isGeneratedValue()) {
-						String alias = column.alias();
-						generatedColumns.add(alias);
-					} else {
-						String table = column.table();
-						if (table == null) {
-							// if it's not a generated column and table is null
-							// then it means the from clause must have a single table
-							if (p.getTables().size() > 1) {
-								throw new RuntimeException("Columns must be preceded by the table name " +
-										"when there are more than one table in the 'from' clause.");
-							}
-							for (var es : p.getTables().entrySet()) table = es.getValue();
-						} else {
-							table = p.getTables().get(table);
-						}
-						System.out.println("table = " + table + ", tableMap.get(table) = " + tableMap.get(table));
-						if (queryImports.add(tableMap.get(table))) {
-							queryImportsStr += "import " + tableMap.get(table) + ";\n";
-							String classTableName = splitPackageClass(tableMap.get(table))[1];
-							String content = """
-									public List<%s> list%s = new ArrayList<>();
-								""".formatted(classTableName, classTableName);
-							queryClassToCreate.content += content;
-						}
-					}
-				}
-
-				if (!generatedColumns.isEmpty()) {
-					queryClassToCreate.content += """
-							public List<%s> generatedColumns = new ArrayList<>();
-						""".formatted(generatedColumnsClassName);
-				}
-
-				queryClassToCreate.content += "}";
-				queryClassToCreate.content = queryClassToCreate.content.replaceAll("#import#", queryImportsStr);
-
-				System.out.println(queryClassToCreate.content);
-				writeBuilderFile(queryClassToCreate);
-
-				if (!generatedColumns.isEmpty()) {
-					// Create GeneratedColumns class
-
-					classContent = """
-						package %s;
-
-						import java.util.List;
-
-						public class %s {
-
-							
-						""".formatted(packageName, generatedColumnsClassName);
-					var generatedColumnsClassToCreate = new ClassToCreate(packageName, generatedColumnsClassName, classContent);
-
-					for (var field : generatedColumns) {
-						String content = "\tpublic Object " + field + ";\n";
-						generatedColumnsClassToCreate.content += content;
-					}
-					generatedColumnsClassToCreate.content += "}";
-					System.out.println(generatedColumnsClassToCreate.content);
-					writeBuilderFile(packageName, generatedColumnsClassName, generatedColumnsClassToCreate.content);
-				}
-
-				// Create map file
-				final String content = createMapResultSetClassForQuery(queryName, packageName, queryClassName, queryImports);
-				writeBuilderFile(packageName, "MapResultSet", content);
-			}
+			processLastRound(annotations, roundEnvironment);
 		} else {
 			processAnnotations(annotations, roundEnvironment);
 		}
@@ -294,38 +300,12 @@ public class MappingProcessor extends AbstractProcessor {
 			System.out.println("Annotated Elements: " + annotatedElements);
 			this.annotatedElements.addAll(annotatedElements);
 			for (var e : annotatedElements) {
-				System.out.println("Element: " + e + " and it's type is " + e.getKind());
-				System.out.println("AnnotationMirrors: " + e.getAnnotationMirrors());
-				for (var am : e.getAnnotationMirrors()) {
-					System.out.println(am.getElementValues());
-				}
+				String elementName = e.toString();
+				System.out.println("Element: " + elementName + " and it's type is " + e.getKind());
+
 				Element enclosingElement = e.getEnclosingElement();
-				String name = e.getSimpleName().toString();
-				System.out.println("element name: " + name);
 				System.out.println("element enclosingElement: " + enclosingElement);
 				System.out.println("element enclosedElements: " + e.getEnclosedElements());
-
-				for (var enclosed : e.getEnclosedElements()) {
-					System.out.println(enclosed + " type is " + enclosed.getKind());
-					System.out.println("Modifiers: " + enclosed.getModifiers());
-					System.out.println(enclosed.getAnnotationMirrors());
-					for (var mirror : enclosed.getAnnotationMirrors()) {
-						DeclaredType annotationType = mirror.getAnnotationType();
-						TypeElement annotationElement = (TypeElement) annotationType.asElement();
-						System.out.println("annotationElement: " + annotationElement);
-					}
-				}
-
-				List<VariableElement> fields = ElementFilter.fieldsIn(e.getEnclosedElements());
-				for (VariableElement field : fields) {
-					TypeMirror fieldType = field.asType();
-					String fullTypeClassName = fieldType.toString();
-					// Validate fullTypeClassName
-					System.out.println(field);
-					System.out.println("fieldType: " + fieldType);
-					System.out.println("fullTypeClassName: " + fullTypeClassName);
-				}
-
 
 				if (e instanceof VariableElement ve) {
 					String query = (String) ve.getConstantValue();
@@ -335,6 +315,13 @@ public class MappingProcessor extends AbstractProcessor {
 					queries.add(e);
 				} else {
 					tables.add(e);
+					List<VariableElement> fields = ElementFilter.fieldsIn(e.getEnclosedElements());
+					List<Field> listField = new ArrayList<>();
+					for (VariableElement field : fields) {
+						String fieldType = field.asType().toString();
+						listField.add(new Field(field.toString(), fieldType));
+					}
+					structures.add(new Structure(elementName, e.getKind().toString(), listField));
 				}
 			}
 		}
--- com/github/mapresultset/Structure.java
+++ com/github/mapresultset/Structure.java
@@ -1,17 +1,34 @@
 package com.github.mapresultset;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 
  */
 public class Structure {
-   public static enum Type {
-       CLASS,
-       RECORD,
-   }
+    public static enum Type {
+        CLASS,
+        RECORD,
+    }
 
-   String fullName;
-   Type type;
-   List<Field> fields;
+    String fullName;
+    Type type;
+    List<Field> fields = new ArrayList<>();
+
+    public Structure(String fullName, String type, List<Field> fields) {
+        this.fullName = fullName;
+        this.type = switch (type) {
+            case "CLASS" -> Type.CLASS;
+            case "RECORD" -> Type.RECORD;
+            default -> throw new RuntimeException("Invalid structure type.");
+        };
+        this.fields = fields;
+    }
+
+    @Override
+    public String toString() {
+        return "Structure [ fullName=" + fullName + ", type=" + type + ", fields=" + fields + "]";
+    }
+
 }
