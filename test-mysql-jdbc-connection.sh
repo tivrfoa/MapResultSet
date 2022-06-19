d6a455397b52b867f077f18084a32b85a6104904
Leandro Coutinho <lescoutinhovr@gmail.com>
2022-06-19 16:51:00 -0300

    fix path in mysql connection test

--- test-mysql-jdbc-connection.sh
+++ test-mysql-jdbc-connection.sh
@@ -2,5 +2,5 @@
 
 ## Testing MySQL JDBC connection
 
-javac -d classes/ org/acme/dao/MySQLCon.java &&
+javac -d classes/ integrationtest/src/test/java/org/acme/dao/MySQLCon.java &&
   java -cp classes/:lib/mysql-connector-java-8.0.29.jar org.acme.dao.MySQLCon
