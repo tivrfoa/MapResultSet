9f1c76dab10bd8800bf2834c782f2ae00e4c8e0a
Leandro Coutinho <lescoutinhovr@gmail.com>
2022-06-18 22:31:41 -0300

    I'm not able to make the annotatio processor to run ...

--- test-processor.sh
+++ test-processor.sh
@@ -1,11 +1,20 @@
 rm -fR classes/* &&
   rm -fR generatedSources/* &&
-  find src/main/java/com/github/tivrfoa/mapresultset/ -name "*.java" > mapsources.txt &&
-  echo '--- Compiling annotation processor sources:' &&
+  find api/src/main/java/com/github/tivrfoa/mapresultset/ -name "*.java" > mapsources.txt &&
+  echo '--- Compiling API:' &&
   echo '--------------' && cat mapsources.txt && echo '---------------' &&
   javac -d classes/ @mapsources.txt &&
-  find src/test/java/org/ -name "*.java" > sources.txt &&
+
+  find processor/src/main/java/com/github/tivrfoa/mapresultset/ -name "*.java" > mapsources.txt &&
+  echo '--- Compiling Processor:' &&
+  echo '--------------' && cat mapsources.txt && echo '---------------' &&
+  javac -d classes/ @mapsources.txt &&
+
+  find integrationtest/src/test/java/org/ -name "*.java" > sources.txt &&
+  echo '--- Compiling Integration Test:' &&
+  echo '--------------' && cat sources.txt && echo '---------------' &&
   javac -cp classes/ -processor com.github.tivrfoa.mapresultset.MappingProcessor -s generatedSources \
-    -d classes/ @sources.txt src/test/java/com/github/tivrfoa/mapresultset/TestProcessor.java &&
+    -d classes/ @sources.txt integrationtest/src/test/java/com/github/tivrfoa/mapresultset/TestProcessor.java &&
+    
   echo '--- Running TestProcessor' &&
   java -cp classes/:lib/mysql-connector-java-8.0.29.jar -ea com.github.tivrfoa.mapresultset.TestProcessor
